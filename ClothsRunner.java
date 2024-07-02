af1ff669f3f835dd4bfbfe411400df835deb501d
RajeshReddy30 <rajeshreddy.xworkz@gmail.com>
2024-07-02 10:32:42 +0530

    methods july 1

---
+++ ClothsRunner.java
@@ -0,0 +1,10 @@
+public class ClothsRunner{
+public static void main(String[] args){
+ClothsRunner.clothBrand();
+Cloths.clothColor();
+ClothsRunner.clothPrice();
+ClothsRunner.getGst(7000);
+ClothsRunner.totalPrice(7000);
+
+}
+}
---
+++ RAJESH.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE colony;
+USE  colony;
+CREATE TABLE house(id int, h_name varchar(10), h_area bigint,h_peoples int);
+CREATE TABLE temple(id int, t_name varchar(20),t_area bigint,t_peoples int);
+CREATE TABLE church(id int,c_name varchar(20),c_area bigint,c_peoples int);
+CREATE DATABASE xworkz;
+USE xworkz;
+CREATE TABLE june_10(id int, j_name varchar(10), j_noof_students bigint, course char);
