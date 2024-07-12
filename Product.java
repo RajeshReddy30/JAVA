48e0d03bd808a0d3ba136bf8ed6364ad4f0ffe65
RajeshReddy30 <rajeshreddy.xworkz@gmail.com>
2024-07-12 10:01:49 +0530

    12th task

--- Product.java
+++ Product.java
@@ -1,16 +1,25 @@
-class Product {
-	public static void main(String args[]){
-    String[] names = {"Laptop", "Smartphone", "Tablet"};
-    int[] stock = {50, 100, 75};
-    long[] ids = {101L, 102L, 103L};
-    char[] ratings = {'A', 'B', 'A'};
+public class Product{
+	public static void product_details(){
+	String product_name="Mobile";
+	int price=150000;
+	System.out.println("Name:"+product_name);
+	System.out.println("Price:"+price);
+	
+	company_details();
+	mobile_details();
+	rating_detail();
+	}
+	public static void company_details(){
+	String mobile_company="Redmi";
+	
+	}
+	public static void mobile_details(){
+	long mi_detail=12345678910L;
+	
+	}
+	public static void rating_detail(){
+	int rating=5;
+	System.out.println("Rating Obtain:"+rating+"Star");
+	}
 
-    for (int s=0; s<names.length; s++) {
-        System.out.println("Product name: " + names[s]);
-        System.out.println("Stock: " + stock[s]);
-        System.out.println("ids: " + ids[s]);
-        System.out.println("ratings: " + ratings[s]);
-		System.out.println("\n");
-    }
-}
 }
